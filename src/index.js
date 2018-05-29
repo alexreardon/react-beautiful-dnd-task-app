be82b6e707f069844548b98ff7f36fa64fdc824d
Alex Reardon <areardon@atlassian.com>
2018-05-29 12:59:30 +1000

    lesson 4

--- src/index.js
+++ src/index.js
@@ -9,7 +9,38 @@ class App extends React.Component {
   state = initialData;
 
   onDragEnd = result => {
-    // TODO: reorder our column
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    this.setState(newState);
   };
 
   render() {
