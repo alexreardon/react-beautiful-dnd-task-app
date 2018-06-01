eda1768fd62f552e277ca6d77871f09a5104c208
Alex Reardon <areardon@atlassian.com>
2018-06-01 16:08:16 +1000

    lesson 11

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ const Container = styled.div`
   display: flex;
 `;
 
+class InnerList extends React.PureComponent {
+  render() {
+    const { column, taskMap, index } = this.props;
+    const tasks = column.taskIds.map(taskId => taskMap[taskId]);
+    return <Column column={column} tasks={tasks} index={index} />;
+  }
+}
+
 class App extends React.Component {
   state = initialData;
 
@@ -106,15 +114,11 @@ class App extends React.Component {
             >
               {this.state.columnOrder.map((columnId, index) => {
                 const column = this.state.columns[columnId];
-                const tasks = column.taskIds.map(
-                  taskId => this.state.tasks[taskId],
-                );
-
                 return (
-                  <Column
+                  <InnerList
                     key={column.id}
                     column={column}
-                    tasks={tasks}
+                    taskMap={this.state.tasks}
                     index={index}
                   />
                 );
